6c738aecc3857647aa5d86f509ae76b03fc1d7a1
Xavier Canchal <xcanchal@usertesting.com>
2025-03-20 16:28:14 +0100

    feat: add netlify for deploying

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,15 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
 
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  adapter: netlify()
 });
